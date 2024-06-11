bab8dd2d9a5146951665f5e3237e1df239335b65
Richard Li <richard@richards-mbp.dhcp.wustl.edu>
2024-06-11 16:12:13 -0500

    please

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Example from './example'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <Example />
+      </DndProvider>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,23 +26,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-*/
-
-
-import { render } from 'react-dom'
-import Example from './example'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-
-function App() {
-  return (
-    <div className="App">
-      <DndProvider backend={HTML5Backend}>
-        <Example />
-      </DndProvider>
-    </div>
-  )
-}
-
-const rootElement = document.getElementById('root')
-render(<App />, rootElement)
