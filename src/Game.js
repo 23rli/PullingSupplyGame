a11b0d3472d6ba645a4c1cbe0401d9c835e1e2ff
Richard Li <richard@Richards-MacBook-Pro.local>
2024-06-11 18:52:29 -0500

    edit

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 export class Game {
   knightPosition = [1, 7]
+
   observers = []
+
   observe(o) {
     this.observers.push(o)
     this.emitChange()
@@ -8,10 +10,12 @@ export class Game {
       this.observers = this.observers.filter((t) => t !== o)
     }
   }
+
   moveKnight(toX, toY) {
     this.knightPosition = [toX, toY]
     this.emitChange()
   }
+
   canMoveKnight(toX, toY) {
     const [x, y] = this.knightPosition
     const dx = toX - x
@@ -21,6 +25,7 @@ export class Game {
       (Math.abs(dx) === 1 && Math.abs(dy) === 2)
     )
   }
+  
   emitChange() {
     const pos = this.knightPosition
     this.observers.forEach((o) => o && o(pos))
