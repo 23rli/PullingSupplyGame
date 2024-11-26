4c4915e8276390ab6b46a310f30abee8b95234b9
23rli <li.richard1@wustl.edu>
2024-11-25 21:19:49 -0500

    trying again

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());                                      // Enable CORS for all rou
 // Database connection pool setup
 var db = mysql.createPool({
   connectionLimit: 100,            // Maximum concurrent connections
-  host: 'localhost',               // Database host
+  host: '3.129.12.15',               // Database host
   user: 'Admin',                    // Database user
   password: 'CarsPullSupplies!',            // Database password
   database: 'MotorCity'            // Target databanpm se
